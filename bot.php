14909b81e892187d045819fc2f1831a0a75fe68c
billiebanditz <32966890+billiebanditz@users.noreply.github.com>
2017-10-22 23:25:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -25,7 +25,7 @@ if($message['type']=='sticker')
 							'messages' => array(
 								array(
 										'type' => 'text',									
-										'text' => 'Terima Kasih Stikernya.'										
+										'text' => 'jangan pake sticker woyyyy.'										
 									
 									)
 							)
@@ -49,7 +49,7 @@ if($url['result'] == 404)
 							'messages' => array(
 								array(
 										'type' => 'text',					
-										'text' => 'Mohon Gunakan Bahasa Indonesia Yang Benar :D.'
+										'text' => 'bahasa paan tuh.'
 									)
 							)
 						);
@@ -66,7 +66,7 @@ if($url['result'] != 100)
 							'messages' => array(
 								array(
 										'type' => 'text',					
-										'text' => 'Maaf '.$profil->displayName.' Server Kami Sedang Sibuk Sekarang.'
+										'text' => 'sorry ya '.$profil->displayName.' gw lg sibuk nih.'
 									)
 							)
 						);
